9e4298b1e4010d8e68f4895f60b9268432582feb
p-gatto <p.gatto@overitech.it>
2025-08-01 20:43:40 +0200

    preparazione configurazione di base

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,18 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+import { MAT_DATE_LOCALE } from '@angular/material/core';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideHttpClient(withInterceptorsFromDi()),
+    { provide: MAT_DATE_LOCALE, useValue: 'it-IT' }
+  ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,41 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+    },
+    /* {
+        path: 'dashboard',
+        loadComponent: () => import('./pages/dashboard/dashboard.component').then(c => c.DashboardComponent)
+    }, */
+    /*  {
+         path: 'invoices',
+         loadChildren: () => import('./features//invoices/invoices.routes').then(r => r.invoicesRoutes)
+     },
+     {
+         path: 'customers',
+         loadComponent: () => import('./pages/customers/customers.component').then(c => c.CustomersComponent)
+     }, */
+    /* {
+        path: 'products',
+        loadComponent: () => import('./pages/products/products.component').then(c => c.ProductsComponent)
+    },
+    {
+        path: 'reports',
+        loadComponent: () => import('./pages/reports/reports.component').then(c => c.ReportsComponent)
+    },
+    {
+        path: 'settings',
+        loadComponent: () => import('./pages/settings/settings.component').then(c => c.SettingsComponent)
+    },
+    {
+        path: 'help',
+        loadComponent: () => import('./pages/help/help.component').then(c => c.HelpComponent)
+    },
+    {
+        path: '**',
+        loadComponent: () => import('./pages/not-found/not-found.component').then(c => c.NotFoundComponent)
+    } */
+];
