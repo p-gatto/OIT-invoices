e952d58157874940b050dd526635682cfd1d6841
p-gatto <p.gatto@overitech.it>
2025-08-01 20:40:47 +0200

    impostato componente principale

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { HeaderComponent } from './core/frame/header/header.component';
+import { FooterComponent } from './core/frame/footer/footer.component';
+import { SidebarComponent } from './core/frame/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'OIT-invoices';
+
+  sidebarOpen = signal(true);
+
+  toggleSidebar() {
+    this.sidebarOpen.update(value => !value);
+  }
+
+  isDesktop(): boolean {
+    return window.innerWidth >= 768;
+  }
 }
