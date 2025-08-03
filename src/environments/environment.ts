f16458f4c1b83292ecdf895042c870c98c8cc98f
p-gatto <p.gatto@overitech.it>
2025-08-03 16:09:33 +0200

    aggiuna gestoine environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    supabaseUrl: 'YOUR_SUPABASE_URL',
-    supabaseKey: 'YOUR_SUPABASE_ANON_KEY'
+    supabaseUrl: 'https://azmyeqtnxecnajeupawk.supabase.co',
+    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF6bXllcXRueGVjbmFqZXVwYXdrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQwNjAyNTcsImV4cCI6MjA2OTYzNjI1N30.Zzc0F0tQtHYA55PBW6FWjEE_6XFz0fNktkhuz2gEXxo'
 };
