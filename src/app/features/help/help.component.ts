0301a46e6be69c7c37554a5220196f178f8abb53
p-gatto <p.gatto@overitech.it>
2025-08-06 15:48:00 +0200

    revisionato help component

--- src/app/features/help/help.component.ts
+++ src/app/features/help/help.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-help',
-  imports: [],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatExpansionModule,
+    MatIconModule,
+    MatButtonModule
+  ],
   templateUrl: './help.component.html',
   styleUrl: './help.component.scss'
 })
